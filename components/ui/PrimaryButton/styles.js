7be18dfeebedb3a9d8a3a6cc1ada40b2342c1d49
ihorborys <ihorborys@gmail.com>
2025-08-26 00:21:50 +0300

    Added PrimaryButton component

---
+++ components/ui/PrimaryButton/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    btn: {
+        width: "100%",
+        backgroundColor: "#00ABC6",
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        alignItems: "center",
+        borderRadius: 12,
+    },
+    btnText: {
+        color: "#FFFFFF",
+        fontSize: 13,
+        fontWeight: 600,
+    },
+})
